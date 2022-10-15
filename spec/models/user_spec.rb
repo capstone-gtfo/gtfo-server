d0f929d406ca96d4f8c001c914c55c6d0c007fa6
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-10-15 15:54:20 -0600

    small change to psuh

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'validations' do
+  describe 'validations in model' do
     it { should validate_presence_of(:longitude) }
     it { should validate_presence_of(:latitude) }
   end
