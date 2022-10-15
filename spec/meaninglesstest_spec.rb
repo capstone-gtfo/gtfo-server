dffd32e5a64450e087984c34d4c6c43661336d23
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-10-15 13:36:11 -0600

    remove test test

--- spec/meaninglesstest_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'C/I Pipelinetest' do
-  it 'Should do the thing for the things we dont know anything about' do
-    expect(circle_ci).to be_successful
-  end
-end
