f9898b85f3088f3079a877ad76adaf9373e04fce
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-10-17 15:11:41 -0500

    refactor user model test
    Co-authored-by: jonathan-pope jonathanmpope@gmail.com
    Co-authored-by: tyler-caudill teecaud@hotmail.com

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:long) }
     it { should validate_presence_of(:lat) }
-    # it { should validate_presence_of(:phone) }
+    it { should validate_length_of(:phone).is_at_least(10).is_at_most(11).on(:create) }
   end
 end
