88bf965ab28d71d61eb36f82a792d0fe5a69d993
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-10-17 14:49:20 -0500

    test/feat users/create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,14 +2,17 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
-      
     binding.pry
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:lat, :long, :phone)
+    params.permit(:lat, :long, :phone)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'validations' do
-    it { should validate_presence_of(:longitude) }
-    it { should validate_presence_of(:latitude) }
+    it { should validate_presence_of(:long) }
+    it { should validate_presence_of(:lat) }
+    it { should validate_presence_of(:phone) }
   end
 end
