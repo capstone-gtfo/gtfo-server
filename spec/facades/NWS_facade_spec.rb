96580b2eaae9aa11df9cd91d076c631bc177d487
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-10-17 17:01:19 -0600

    add api/v1 file stucture and controller file
    Co-authored-by: Nick <nickjones9605@gmail.com>

--- spec/facades/NWS_facade_spec.rb
+++ spec/facades/NWS_facade_spec.rb
@@ -8,11 +8,4 @@ RSpec.describe NWSFacade do
         expect(response).to be_a(Array)
         expect(response.first).to be_a(Disaster)
     end
-
-    it 'sad path', :vcr do
-        location = "null"
-        response = NWSFacade.retrieve_disaster(location)
-        
-        expect(response.empty?).to be(true)
-    end
 end
