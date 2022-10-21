bf40c10c438c08a5d091a9b4dfc43d97efd1feee
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-10-21 12:18:08 -0600

    change directory name

--- spec/services/NWS_service_spec.rb
+++ spec/services/NWS_service_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'NWSService' do
     it 'gets weather alerts' do
-        #@disasters = JSON.parse(File.read('spec/fixtures/disaster_data.json'), symbolize_names: true)
-        #allow(NWSService).to receive(:get_disaster).and_return(@disasters)
+        @disasters = JSON.parse(File.read('spec/fixtures/disaster_data.json'), symbolize_names: true)
+        allow(NWSService).to receive(:get_disaster).and_return(@disasters)
 
         location = "29.008056,-81.382778"
         response = NWSService.get_disaster(location)
-
+        
         expect(response).to be_a(Hash)
     end
 end
