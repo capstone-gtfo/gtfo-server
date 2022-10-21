d2659e9a76cb8fa31334b73d92537bb09116b38c
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-10-21 11:11:10 -0600

    change service

--- app/services/NWS_service.rb
+++ app/services/NWS_service.rb
@@ -1,12 +1,12 @@
 class NWSService
  
     def self.get_disaster(location)
-        response = conn.get("/alerts/active") do |faraday|
-            faraday.params['point'] = location
-            faraday.params['status'] = 'actual'
-            faraday.params['urgency'] = 'Immediate,Expected'
-            faraday.params['severity'] = 'Extreme,Severe'
-            faraday.params['certainty'] = 'Observed,Likely'
+        response = conn.get("/alerts/active?point=#{location}&status=actual") do |faraday|
+            #faraday.params['point'] = location
+            #faraday.params['status'] = 'actual'
+            faraday.params['urgency'] = %w[Immediate Expected]
+            faraday.params['severity'] = %w[Extreme Severe]
+            faraday.params['certainty'] = %w[Observed Likely]
         end
         result = JSON.parse(response.body, symbolize_names: true) 
       end
